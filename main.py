6c9d9e549fa3de777789de6a720752c024dadd7e
Jacob Sayles <jsayles@gmail.com>
2017-09-09 07:40:28 -0700

    clean up

--- main.py
+++ main.py
@@ -6,20 +6,46 @@ try:
 except ImportError:
     PI = False
 
+import os
+import sys
 import logging
 import time
-import os
-from watcher import Watcher
-from printer import Printer
-from httpd.server import Server
-from video import SLIDE
-import sys
 import traceback
+
 import settings
+from rotary.rotary_thread import RotaryThread
+#from httpd.server import Server
+
+logger = logging.getLogger('piradio')
+
+######################################################################
+# Callbacks
+######################################################################
+
+def rotary1_left():
+    logger.debug("rotary1_left")
+
+def rotary1_right():
+    logger.debug("rotary1_right")
+
+def rotary1_push():
+    logger.debug("rotary1_push")
+
+def rotary2_left():
+    logger.debug("rotary2_left")
+
+def rotary2_right():
+    logger.debug("rotary2_right")
+
+def rotary2_push():
+    logger.debug("rotary2_push")
+
+######################################################################
+# Main Loop
+######################################################################
 
 def main():
     # Setup Logging
-    logger = logging.getLogger('piradio')
     hdlr = logging.FileHandler(settings.LOG)
     formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
     hdlr.setFormatter(formatter)
@@ -37,73 +63,50 @@ def main():
         logger.info("Starting up...")
 
     if PI:
-        # Not interested
         GPIO.setwarnings(False)
-
-        # Use BCM GPIO numbers
         GPIO.setmode(GPIO.BCM)
+        # Rotary Encoder 1
+        GPIO.setup(settings.ROTARY1_APin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(settings.ROTARY1_BPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(settings.ROTARY1_SPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        # Rotary Encoder 2
+        GPIO.setup(settings.ROTARY2_APin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(settings.ROTARY2_BPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(settings.ROTARY2_SPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-        # Setup the alert light
-        if settings.LIGHT_PIN_1:
-        GPIO.setup(settings.LIGHT_PIN_1, GPIO.OUT)
-        GPIO.output(settings.LIGHT_PIN_1, GPIO.LOW)
+        rotary1_thread = None
+        rotary2_thread = None
+        # server = None
 
-        if settings.LIGHT_PIN_2:
-        GPIO.setup(settings.LIGHT_PIN_2, GPIO.OUT)
-        GPIO.output(settings.LIGHT_PIN_2, GPIO.LOW)
-
-
-        # The queue is where messages go to be displayed
-        queue = Queue.PriorityQueue()
-
-        watcher = None
-        printer = None
-        server = None
         loops = 0
         while True:
-        try:
-        loops = loops + 1
-        logger.debug("Main Loop " + str(loops))
+            try:
+                loops = loops + 1
+                logger.debug("Main Loop " + str(loops))
 
-        # Make sure our twitter thread is alive and happy
-        if not watcher or not watcher.is_alive():
-        logger.info("Starting watcher thread")
-        watcher = Watcher(queue, logger)
-        watcher.setDaemon(True)
-        watcher.start()
+                # Make sure our rotary threads are alive and happy
+                if not rotary1_thread or not rotary1_thread.is_alive():
+                    logger.info("Starting rotary1 thread")
+                    rotary1_thread = RotaryThread(settings.ROTARY1_APin, settings.ROTARY1_BPin, settings.ROTARY1_SPin)
+                    rotary1_thread.setLeftCallback(rotary1_left)
+                    rotary1_thread.setRightCallbck(rotary1_right)
+                    rotary1_thread.setPushCallback(rotary1_push)
+                    rotary1_thread.setDaemon(True)
+                    rotary1_thread.start()
 
-        # Make sure our printing thread is alive and happy
-        if not printer or not printer.is_alive():
-        logger.info("Starting printer thread")
-        printer = Printer(queue, logger, PI)
-        printer.setDaemon(True)
-        printer.start()
+                    # if settings.HTTP_SERVER:
+                    #     if not server or not server.is_alive():
+                    #         logger.info("Starting HTTP server")
+                    #         server = Server(queue, logger)
+                    #         server.setDaemon(True)
+                    #         server.start()
 
-        if settings.HTTP_SERVER:
-        if not server or not server.is_alive():
-        logger.info("Starting HTTP server")
-        server = Server(queue, logger)
-        server.setDaemon(True)
-        server.start()
+                    time.sleep(15)
 
-
-        # Throw some info in the queue if it's getting low
-        if queue.qsize() < 1:
-        messages = open(settings.MSG_FILE, 'r')
-        for msg in messages:
-        queue.put((settings.PRIORITY_LOW, msg, "", False))
-        messages.close()
-
-        if settings.SLIDE_DIR:
-        for filename in os.listdir(settings.SLIDE_DIR):
-        queue.put((settings.PRIORITY_LOW, os.path.join(settings.SLIDE_DIR, filename), SLIDE, False))
-        except Exception as e:
-        logger.exception("Exception in main thread: " + str(e))
-
-        time.sleep(15)
-
-        logger.warn("Exiting main thread")
-
+                    logger.warn("Exiting main thread")
+            finally:
+                # Release resource
+                GPIO.cleanup()
 
 if __name__ == '__main__':
     main()
